5fd337b4c131504c46161f66ab91f73cec2f0caf
Sofia Adorni Braccesi <sofiaadornibraccesi@---.homenet.telecomitalia.it>
2021-03-12 20:09:50 +0100

    Adding speech synthesis

---
+++ utils/SpeechSynthesis.py
@@ -0,0 +1,9 @@
+import pyttsx3
+
+def say(text):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.say(text)
+    engine.runAndWait()
+    print(voices[0].id)
